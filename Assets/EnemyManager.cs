04c2cb68ea05675731c0e6082e6e04442b371899
MKhang06 <phamminhkhang081120@gmail.com>
2026-04-07 09:51:14 +0700

    hhh

--- Assets/EnemyManager.cs
+++ Assets/EnemyManager.cs
@@ -8,7 +8,7 @@ public class EnemyManager : MonoBehaviour
     //Hãy sửa lại dòng nãy hợp lý, mỗi thành viên sẽ thay phiên nhau dùng int hoặc float
     //Sau đó kiểm tra những phần còn thiếu khác để script có thể hoạt động bình thường
 
-    // public speed;
+    public int speed;
 
     private Transform targetPoint;
     private bool isFacingRight = true;
@@ -20,11 +20,11 @@ public class EnemyManager : MonoBehaviour
 
     void Update()
     {
-        // transform.position = Vector2.MoveTowards(
-            // transform.position,
-            // targetPoint.position,
-            // speed * Time.deltaTime
-        // );
+        transform.position = Vector2.MoveTowards(
+            transform.position,
+            targetPoint.position,
+            speed * Time.deltaTime
+        );
 
         if (Vector2.Distance(transform.position, targetPoint.position) < 0.1f)
         {
